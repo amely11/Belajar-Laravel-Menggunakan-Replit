114a4dc3d4d2277c7fa65e9cefdb6fe1b5f503f6
amely11 <teresiaamelia12@gmail.com>
2024-02-28 15:31:14 +0700

    Update font body
    Update font body

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
 
         <style>
             body {
-                font-family: 'Nunito', sans-serif;
+                font-family: 'Poppins', sans-serif;
             }
         </style>
     </head>
